a983ea11a71b8ff00af733ef1f444364abb06e95
bankba <bankbav3@gmail.com>
2017-09-22 13:51:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,11 +15,16 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+                        
+			$ans="รวย";
+			if($text=="สายพิณ")
+			   $ans="แก้วนิ่ม";
+			else if($text=="ศิโรจน์")
+		           $ans="น้อยขุ่ย";
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text."แก้วนิ่ม"
+				'text' => $text." ".$ans
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
