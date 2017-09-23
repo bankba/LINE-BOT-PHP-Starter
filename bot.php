382fdbe7e486960346d0f249a06c51403ee78fe4
bankba <bankbav3@gmail.com>
2017-09-23 15:01:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,8 +11,8 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			{
+		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
+			
 				$replyToken = $event['replyToken'];
 				$messages = [
 				'type' => 'text',
